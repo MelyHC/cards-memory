8e500a46680fc1a4c9406d5e3c3ff0c2aeae2a40
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-12-28 02:47:08 -0500

    Updates

---
+++ precache-manifest.b3207892eb14f13b50b29902be81725f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d99895b678b39c67a67d3d73cd9686",
+    "url": "/cards-memory/static/media/back.a7d99895.PNG"
+  },
+  {
+    "revision": "7a8f5a5dd279606687c3",
+    "url": "/cards-memory/static/js/runtime~main.7a8f5a5d.js"
+  },
+  {
+    "revision": "03cc64c896737f745a9c",
+    "url": "/cards-memory/static/js/main.03cc64c8.chunk.js"
+  },
+  {
+    "revision": "6f4e42fbaff8fc84a3b6",
+    "url": "/cards-memory/static/js/1.6f4e42fb.chunk.js"
+  },
+  {
+    "revision": "03cc64c896737f745a9c",
+    "url": "/cards-memory/static/css/main.039aa132.chunk.css"
+  },
+  {
+    "revision": "44153feb2d37dc78728df194a910914e",
+    "url": "/cards-memory/index.html"
+  }
+];
