c446bca41b007a0020237c4f441ae3fa380c2332
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-12-28 01:12:11 -0500

    Agregando nivel y mensaje final

--- src/App.js
+++ src/App.js
@@ -8,52 +8,72 @@ class App extends Component {
   state = {
     allCards: [],
     count: 0,
-    level: null,
+    level: 0,
     cards: [],
     compareCards: []
   }
 
   comparingCards = (arrCards, currentCount) => {
-    let { compareCards } = this.state;
     const { cards } = this.state;
-    cards.forEach(card => {
-      arrCards.forEach(currentCard => {
-        if(card.img === currentCard.img) {
-          card.visibility = true;
+    const [firstCard, secondCard] = arrCards;
+
+    setTimeout(() => {
+      cards.forEach(card => {
+        if (firstCard.img === secondCard.img) {
+          if (card.index === firstCard.index || card.index === secondCard.index) {
+            card.visibility = true;
+          }
         } else {
-          card.flip = false
+          card.flip = false;
         }
       })
-    })
 
-    compareCards = [];
-    this.setState({ count: currentCount++, compareCards })
+      this.setState({ count: currentCount + 1 })
+    }, 1000)
+
   }
 
   handleClick = (i) => {
-    const { cards, compareCards } = this.state;
-    let { count } = this.state;
+    const { cards } = this.state;
+    let { count, compareCards } = this.state;
 
     cards.forEach(card => {
       if (card.index === i) {
         card.flip = !card.flip;
         if (!compareCards.find(cardCurrent => card === cardCurrent)) {
           compareCards.push(card);
-          if (compareCards.length+1 <3) {
-            this.comparingCards(compareCards, count)
+          if (compareCards.length === 2) {
+            this.comparingCards(compareCards, count);
+            compareCards = [];
           }
         }
       }
     })
 
-    this.setState({ cards })
+    this.setState({ cards, compareCards })
   }
 
   refreshPage = () => window.location.reload();
 
-  componentWillMount() {
-    const { cards } = this.state;
+  selectLevel = (e) => {
+    const { allCards, cards } = this.state;
+    e.preventDefault();
+    const levelSelected = parseInt(e.target.value);
 
+    while (cards.length < levelSelected) {
+      const selectCard = allCards[Math.round(Math.random() * allCards.length)];
+      if (!cards.find(card => card === selectCard)) {
+        cards.push(selectCard);
+        cards.push({ ...selectCard, index: selectCard.index + 1 });
+      }
+    }
+
+    suffle(cards);
+
+    this.setState({ cards, level: levelSelected })
+  }
+
+  componentWillMount() {
     const arrCards = data.map(({ name, img }, index) => {
       return {
         name,
@@ -64,65 +84,56 @@ class App extends Component {
       }
     });
 
-    while (cards.length < 8) {
-      const selectCard = arrCards[Math.round(Math.random() * arrCards.length)];
-      if (!cards.find(card => card === selectCard)) {
-        cards.push(selectCard);
-        cards.push({ ...selectCard, index: selectCard.index + 1 });
-      }
-    }
-
-    suffle(cards)
-
-    this.setState({ allCards: data, cards })
+    this.setState({ allCards: arrCards })
   }
 
   render() {
-    const { cards, count } = this.state;
+    const { cards, count, level } = this.state;
 
-    return (
-      <Fragment>
-        <header className="d-flex justify-content-between bg-black text-white">
-          <i className="fas fa-undo-alt btn" onClick={this.refreshPage} ></i>
-          <h4 className="d-inline-block text-center">Cards Memory</h4>
-          <h5 className="m-1">N°: {count}</h5>
-        </header>
-        <div className="modal" tabIndex="-1" role="dialog" aria-hidden="false">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Modal title</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                ...
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Save changes</button>
-              </div>
-            </div>
-          </div>
+    if (level === 0) {
+      return (
+        <div className="home d-flex justify-content-center align-items-center bg-black">
+          <section className="bg-light px-2 py-5 rounded">
+            <h5 className="m-2">Elige un nivel para comenzar</h5>
+            <form onClick={this.selectLevel} className="d-flex justify-content-center mt-4">
+              <button className="btn btn-primary m-2" value="8">Fácil</button>
+              <button className="btn btn-warning text-white m-2" value="16">Medio</button>
+              <button className="btn btn-danger m-2" value="32">Difícil</button>
+            </form>
+          </section>
         </div>
-        <div className="container">
-          <div className="row m-0 p-0 view justify-content-center">
-            {
-              cards.map(({ name, img, visibility, flip, index }, i) =>
-                <div className="col-6 col-sm-4 col-md-3 my-1 content-card" key={index + i}>
-                  <figure className={`card-flip ${flip ? "is-flipped" : null}`} onClick={() => this.handleClick(index)}>
-                    <img className="card-face card-face-front img-fluid rounded" src={img} alt={name}></img>
-                    <img className="card-face img-fluid rounded" src={back} alt="parte trasera de la carta"></img>
-                  </figure>
-                </div>
-              )
+      )
+    } else {
+      return (
+        <Fragment>
+          <header className="d-flex justify-content-between bg-black text-white">
+            <i className="fas fa-undo-alt btn icon-white" onClick={this.refreshPage}></i>
+            <h4 className="d-inline-block text-center">Cards Memory</h4>
+            <h5 className="m-1">N°: {count}</h5>
+          </header>
+          <div className="container">
+            {cards.every(cardHide => cardHide.visibility === true)
+              ? <div className="d-flex view justify-content-center align-items-center">
+                <section className="rounded bg-light text-center p-2">
+                  <h5 className="m-4">¡Felicidades ganaste!</h5>
+                  <p>En {count} intentos <i className="fas fa-undo-alt btn" onClick={this.refreshPage}></i></p>
+                </section>
+              </div>
+              : <div className="row m-0 p-0 view justify-content-center">
+                {cards.map(({ name, img, visibility, flip, index }, i) =>
+                  <div className="col-6 col-sm-4 col-md-3 my-1 content-card" key={index + i}>
+                    <figure className={`card-flip ${flip ? "is-flipped" : null} ${visibility ? "hidden" : null}`} onClick={() => this.handleClick(index)}>
+                      <img className="card-face card-face-front img-fluid rounded" src={img} alt={name}></img>
+                      <img className="card-face img-fluid rounded" src={back} alt="parte trasera de la carta"></img>
+                    </figure>
+                  </div>
+                )}
+              </div>
             }
           </div>
-        </div>
-
-      </Fragment>
-    );
+        </Fragment>
+      )
+    }
   }
 }
 
