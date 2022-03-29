8401313243564611275e2c00bac9dadcc28b576b
MelyHC <mely.hidalgo.crespo@gmail.com>
2022-03-29 11:05:14 -0500

    cambio diseño cartas

---
+++ src/dataCards/dataCardsBzV2.js
@@ -0,0 +1,131 @@
+import Card1 from '../BZRecuerdo/BzCarta/01.png';
+import Card2 from '../BZRecuerdo/BzCarta/02.png';
+import Card3 from '../BZRecuerdo/BzCarta/03.png';
+import Card4 from '../BZRecuerdo/BzCarta/04.png';
+import Card5 from '../BZRecuerdo/BzCarta/05.png';
+import Card6 from '../BZRecuerdo/BzCarta/06.png';
+import Card7 from '../BZRecuerdo/BzCarta/07.png';
+import Card8 from '../BZRecuerdo/BzCarta/08.png';
+import Card9 from '../BZRecuerdo/BzCarta/09.png';
+import Card10 from '../BZRecuerdo/BzCarta/10.png';
+import Card11 from '../BZRecuerdo/BzCarta/11.png';
+import Card12 from '../BZRecuerdo/BzCarta/12.png';
+import Card13 from '../BZRecuerdo/BzCarta/13.png';
+import Card14 from '../BZRecuerdo/BzCarta/14.png';
+import Card15 from '../BZRecuerdo/BzCarta/015.png';
+import Card16 from '../BZRecuerdo/BzCarta/016.png';
+import Card17 from '../BZRecuerdo/BzCarta/017.png';
+import Card18 from '../BZRecuerdo/BzCarta/018.png';
+import Card19 from '../BZRecuerdo/BzCarta/019.png';
+import Card20 from '../BZRecuerdo/BzCarta/020.png';
+import Card21 from '../BZRecuerdo/BzCarta/021.png';
+import Card22 from '../BZRecuerdo/BzCarta/022.png';
+import Card23 from '../BZRecuerdo/BzCarta/023.png';
+import Card24 from '../BZRecuerdo/BzCarta/024.png';
+import Card25 from '../BZRecuerdo/BzCarta/025.png';
+import Card26 from '../BZRecuerdo/BzCarta/026.png';
+import Card27 from '../BZRecuerdo/BzCarta/027.png';
+import Card28 from '../BZRecuerdo/BzCarta/028.png';
+import Card29 from '../BZRecuerdo/BzCarta/029.png';
+import Card30 from '../BZRecuerdo/BzCarta/030.png';
+import Card31 from '../BZRecuerdo/BzCarta/031.png';
+import Card32 from '../BZRecuerdo/BzCarta/032.png';
+
+export default [
+  {
+    "img": Card1,
+  },
+  {
+    "img": Card2,
+  },
+  {
+    "img": Card3,
+  },
+  {
+    "img": Card4,
+  },
+  {
+    "img": Card5,
+  },
+  {
+    "img": Card6,
+  },
+  {
+    "img": Card7,
+  },
+  {
+    "img": Card8,
+  },
+  {
+    "img": Card9,
+  },
+  {
+    "img": Card10,
+  },
+  {
+    "img": Card11,
+  },
+  {
+    "img": Card12,
+  },
+  {
+    "img": Card13,
+  },
+  {
+    "img": Card14,
+  },
+  {
+    "img": Card15,
+  },
+  {
+    "img": Card16,
+  },
+  {
+    "img": Card17,
+  },
+  {
+    "img": Card18,
+  },
+  {
+    "img": Card19,
+  },
+  {
+    "img": Card20,
+  },
+  {
+    "img": Card21,
+  },
+  {
+    "img": Card22,
+  },
+  {
+    "img": Card23,
+  },
+  {
+    "img": Card24,
+  },
+  {
+    "img": Card25,
+  },
+  {
+    "img": Card26,
+  },
+  {
+    "img": Card27,
+  },
+  {
+    "img": Card28,
+  },
+  {
+    "img": Card29,
+  },
+  {
+    "img": Card30,
+  },
+  {
+    "img": Card31,
+  },
+  {
+    "img": Card32,
+  }
+];
